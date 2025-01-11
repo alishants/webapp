cd537eca346cd79c0dbde6386f30b0c435883796
alishants <67666315+alishants@users.noreply.github.com>
2025-01-11 02:39:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const fetch = require('node-fetch');  // Здесь мы используем require для node-fetch версии 2.x
 
-const token = '7433319114:AAEzW9uU3lQ3l7SZtiR8m60s9Z1FZ9kKHZU';  // Твой токен бота
-const chatId = '591775682';   // Идентификатор чата, куда будет отправлено сообщение
+const token = '';  // Твой токен бота
+const chatId = '';   // Идентификатор чата, куда будет отправлено сообщение
 
 const url = `https://api.telegram.org/bot${token}/sendMessage`;
 
 const button = {
   text: 'Open Web App',
   web_app: {
-    url: 'https://app.lava.top/topone?subscriptionOfferId=b44dd6b0-dddc-4e1b-8ae6-b4601577e822'  // Укажи ссылку на свое веб-приложение
+    url: ''  // Укажи ссылку на свое веб-приложение
   }
 };
 
